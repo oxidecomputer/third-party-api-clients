9da801aa8505f0b506c0b7dbaffef986d063abf4
Jess Frazelle <jess@oxide.computer>
2021-09-03 15:28:15 -0700

    bump version
    Signed-off-by: Jess Frazelle <jess@oxide.computer>

--- github/src/lib.rs
+++ github/src/lib.rs
@@ -35,7 +35,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! octorust = "0.1.28"
+//! octorust = "0.1.29"
 //! ```
 //!
 //! ## Basic example
@@ -53,7 +53,7 @@
 //! ```
 //!
 //! If you are a GitHub enterprise customer, you will want to create a client with the
-//! [Client#host](https://docs.rs/octorust/0.1.28/octorust/struct.Client.html#method.host) method.
+//! [Client#host](https://docs.rs/octorust/0.1.29/octorust/struct.Client.html#method.host) method.
 //!
 //! ## Feature flags
 //!
@@ -67,7 +67,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! octorust = { version = "0.1.28", features = ["httpcache"] }
+//! octorust = { version = "0.1.29", features = ["httpcache"] }
 //! ```
 //!
 //! Then use the `Client::custom` constructor to provide a cache implementation.
