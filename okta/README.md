fe698ce5c1fe99e874a9348d966bcaf7e2ab922d
Jess Frazelle <jess@oxide.computer>
2021-09-22 14:48:47 -0700

    update okta
    Signed-off-by: Jess Frazelle <jess@oxide.computer>

--- okta/README.md
+++ okta/README.md
@@ -49,56 +49,19 @@ a user agent string and set of credentials.
 use okta::Client;
 
 let okta = Client::new(
-    String::from("client-id"),
-    String::from("client-secret"),
-    String::from("redirect-uri"),
-    String::from("token"),
-    String::from("refresh-token")
+    String::from("api-key"),
 );
 ```
 
 Alternatively, the library can search for most of the variables required for
 the client in the environment:
 
-- `OKTA_CLIENT_ID`
-- `OKTA_CLIENT_SECRET`
-- `OKTA_REDIRECT_URI`
+- `OKTA_API_KEY`
 
 And then you can create a client from the environment.
 
 ```
 use okta::Client;
 
-let okta = Client::new_from_env(
-    String::from("token"),
-    String::from("refresh-token")
-);
-```
-
-It is okay to pass empty values for `token` and `refresh_token`. In
-the initial state of the client, you will not know these values.
-
-To start off a fresh client and get a `token` and `refresh_token`, use the following.
-
-```
-use okta::Client;
-
-async fn do_call() {
-    let mut okta = Client::new_from_env("", "");
-
-    // Get the URL to request consent from the user.
-    // You can optionally pass in scopes. If none are provided, then the
-    // resulting URL will not have any scopes.
-    let user_consent_url = okta.user_consent_url(&["some-scope".to_string()]);
-
-    // In your redirect URL capture the code sent and our state.
-    // Send it along to the request for the token.
-    let code = "thing-from-redirect-url";
-    let state = "state-from-redirect-url";
-    let mut access_token = okta.get_access_token(code, state).await.unwrap();
-
-    // You can additionally refresh the access token with the following.
-    // You must have a refresh token to be able to call this function.
-    access_token = okta.refresh_access_token().await.unwrap();
-}
+let okta = Client::new_from_env();
 ```
--- okta/src/lib.rs
+++ okta/src/lib.rs
@@ -46,56 +46,20 @@
 //! ```
 //! use okta::Client;
 //!
-//! let okta = Client::new(
-//!     String::from("client-id"),
-//!     String::from("client-secret"),
-//!     String::from("redirect-uri"),
-//!     String::from("token"),
-//!     String::from("refresh-token"),
-//! );
+//! let okta = Client::new(String::from("api-key"));
 //! ```
 //!
 //! Alternatively, the library can search for most of the variables required for
 //! the client in the environment:
 //!
-//! - `OKTA_CLIENT_ID`
-//! - `OKTA_CLIENT_SECRET`
-//! - `OKTA_REDIRECT_URI`
+//! - `OKTA_API_KEY`
 //!
 //! And then you can create a client from the environment.
 //!
 //! ```
 //! use okta::Client;
 //!
-//! let okta = Client::new_from_env(String::from("token"), String::from("refresh-token"));
-//! ```
-//!
-//! It is okay to pass empty values for `token` and `refresh_token`. In
-//! the initial state of the client, you will not know these values.
-//!
-//! To start off a fresh client and get a `token` and `refresh_token`, use the following.
-//!
-//! ```
-//! use okta::Client;
-//!
-//! async fn do_call() {
-//!     let mut okta = Client::new_from_env("", "");
-//!
-//!     // Get the URL to request consent from the user.
-//!     // You can optionally pass in scopes. If none are provided, then the
-//!     // resulting URL will not have any scopes.
-//!     let user_consent_url = okta.user_consent_url(&["some-scope".to_string()]);
-//!
-//!     // In your redirect URL capture the code sent and our state.
-//!     // Send it along to the request for the token.
-//!     let code = "thing-from-redirect-url";
-//!     let state = "state-from-redirect-url";
-//!     let mut access_token = okta.get_access_token(code, state).await.unwrap();
-//!
-//!     // You can additionally refresh the access token with the following.
-//!     // You must have a refresh token to be able to call this function.
-//!     access_token = okta.refresh_access_token().await.unwrap();
-//! }
+//! let okta = Client::new_from_env();
 //! ```
 #![feature(async_stream)]
 #![allow(clippy::too_many_arguments)]
@@ -158,99 +122,31 @@ mod progenitor_support {
 
 use std::env;
 
-const TOKEN_ENDPOINT: &str = "https://account.okta.com/oauth/token";
-const USER_CONSENT_ENDPOINT: &str = "https://account.okta.com/oauth/auth";
-
 /// Entrypoint for interacting with the API client.
 #[derive(Clone)]
 pub struct Client {
     host: String,
     token: String,
-    // This will expire within a certain amount of time as determined by the
-    // expiration date passed back in the initial request.
-    refresh_token: String,
-    client_id: String,
-    client_secret: String,
-    redirect_uri: String,
 
     client: reqwest::Client,
-}
-
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, JsonSchema, Clone, Default, Serialize, Deserialize)]
-pub struct AccessToken {
-    #[serde(
-        default,
-        skip_serializing_if = "String::is_empty",
-        deserialize_with = "crate::utils::deserialize_null_string::deserialize"
-    )]
-    pub token_type: String,
-
-    #[serde(
-        default,
-        skip_serializing_if = "String::is_empty",
-        deserialize_with = "crate::utils::deserialize_null_string::deserialize"
-    )]
-    pub access_token: String,
-    #[serde(default)]
-    pub expires_in: i64,
-
-    #[serde(
-        default,
-        skip_serializing_if = "String::is_empty",
-        deserialize_with = "crate::utils::deserialize_null_string::deserialize"
-    )]
-    pub refresh_token: String,
-    #[serde(default, alias = "x_refresh_token_expires_in")]
-    pub refresh_token_expires_in: i64,
-
-    #[serde(
-        default,
-        skip_serializing_if = "String::is_empty",
-        deserialize_with = "crate::utils::deserialize_null_string::deserialize"
-    )]
-    pub scope: String,
 }
 
 impl Client {
     /// Create a new Client struct. It takes a type that can convert into
     /// an &str (`String` or `Vec<u8>` for example). As long as the function is
     /// given a valid API key your requests will work.
-    pub fn new<I, K, R, T, Q>(
-        client_id: I,
-        client_secret: K,
-        redirect_uri: R,
-        token: T,
-        refresh_token: Q,
-    ) -> Self
+    pub fn new<T>(token: T) -> Self
     where
-        I: ToString,
-        K: ToString,
-        R: ToString,
         T: ToString,
-        Q: ToString,
     {
         let client = reqwest::Client::builder().build();
         match client {
-            Ok(c) => {
-                // We do not refresh the access token here since we leave that up to the
-                // user to do so they can re-save it to their database.
-                // TODO: But in the future we should save the expires in date and refresh it
-                // if it needs to be refreshed.
-                //
-                Client {
-                    host: DEFAULT_HOST.to_string(),
-                    client_id: client_id.to_string(),
-                    client_secret: client_secret.to_string(),
-                    redirect_uri: redirect_uri.to_string(),
-                    token: token.to_string(),
-                    refresh_token: refresh_token.to_string(),
+            Ok(c) => Client {
+                host: DEFAULT_HOST.to_string(),
+                token: token.to_string(),
 
-                    client: c,
-                }
-            }
+                client: c,
+            },
             Err(e) => panic!("creating reqwest client failed: {:?}", e),
         }
     }
@@ -271,113 +167,16 @@ impl Client {
     /// given a valid API key and your requests will work.
     /// We pass in the token and refresh token to the client so if you are storing
     /// it in a database, you can get it first.
-    pub fn new_from_env<T, R>(token: T, refresh_token: R) -> Self
-    where
-        T: ToString,
-        R: ToString,
-    {
-        let client_id = env::var("OKTA_CLIENT_ID").expect("must set OKTA_CLIENT_ID");
-        let client_secret = env::var("OKTA_CLIENT_SECRET").expect("must set OKTA_CLIENT_SECRET");
-        let redirect_uri = env::var("OKTA_REDIRECT_URI").expect("must set OKTA_REDIRECT_URI");
+    pub fn new_from_env() -> Self {
+        let token = env::var("OKTA_API_KEY").expect("must set OKTA_API_KEY");
 
-        Client::new(client_id, client_secret, redirect_uri, token, refresh_token)
-    }
-
-    /// Return a user consent url with an optional set of scopes.
-    /// If no scopes are provided, they will not be passed in the url.
-    pub fn user_consent_url(&self, scopes: &[String]) -> String {
-        let state = uuid::Uuid::new_v4();
-
-        let url = format!(
-            "{}?client_id={}&response_type=code&redirect_uri={}&state={}",
-            USER_CONSENT_ENDPOINT, self.client_id, self.redirect_uri, state
-        );
-
-        if scopes.is_empty() {
-            return url;
-        }
-
-        // Add the scopes.
-        format!("{}&scope={}", url, scopes.join(" "))
-    }
-
-    /// Refresh an access token from a refresh token. Client must have a refresh token
-    /// for this to work.
-    pub async fn refresh_access_token(&mut self) -> Result<AccessToken> {
-        if self.refresh_token.is_empty() {
-            anyhow!("refresh token cannot be empty");
-        }
-
-        let mut headers = reqwest::header::HeaderMap::new();
-        headers.append(
-            reqwest::header::ACCEPT,
-            reqwest::header::HeaderValue::from_static("application/json"),
-        );
-
-        let params = [
-            ("grant_type", "refresh_token"),
-            ("refresh_token", &self.refresh_token),
-            ("client_id", &self.client_id),
-            ("client_secret", &self.client_secret),
-            ("redirect_uri", &self.redirect_uri),
-        ];
-        let client = reqwest::Client::new();
-        let resp = client
-            .post(TOKEN_ENDPOINT)
-            .headers(headers)
-            .form(&params)
-            .basic_auth(&self.client_id, Some(&self.client_secret))
-            .send()
-            .await?;
-
-        // Unwrap the response.
-        let t: AccessToken = resp.json().await?;
-
-        self.token = t.access_token.to_string();
-        self.refresh_token = t.refresh_token.to_string();
-
-        Ok(t)
-    }
-
-    /// Get an access token from the code returned by the URL paramter sent to the
-    /// redirect URL.
-    pub async fn get_access_token(&mut self, code: &str, state: &str) -> Result<AccessToken> {
-        let mut headers = reqwest::header::HeaderMap::new();
-        headers.append(
-            reqwest::header::ACCEPT,
-            reqwest::header::HeaderValue::from_static("application/json"),
-        );
-
-        let params = [
-            ("grant_type", "authorization_code"),
-            ("code", code),
-            ("client_id", &self.client_id),
-            ("client_secret", &self.client_secret),
-            ("redirect_uri", &self.redirect_uri),
-            ("state", state),
-        ];
-        let client = reqwest::Client::new();
-        let resp = client
-            .post(TOKEN_ENDPOINT)
-            .headers(headers)
-            .form(&params)
-            .basic_auth(&self.client_id, Some(&self.client_secret))
-            .send()
-            .await?;
-
-        // Unwrap the response.
-        let t: AccessToken = resp.json().await?;
-
-        self.token = t.access_token.to_string();
-        self.refresh_token = t.refresh_token.to_string();
-
-        Ok(t)
+        Client::new(token)
     }
 
     async fn url_and_auth(&self, uri: &str) -> Result<(reqwest::Url, Option<String>)> {
         let parsed_url = uri.parse::<reqwest::Url>();
 
-        let auth = format!("Bearer {}", self.token);
+        let auth = format!("SSWS {}", self.token);
         parsed_url.map(|u| (u, Some(auth))).map_err(Error::from)
     }
 
