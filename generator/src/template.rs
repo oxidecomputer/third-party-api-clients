6127d303584029c66a5b3d7802a0577058c55a27
Augustus Mayo <gusmayo@gmail.com>
2023-02-14 16:17:28 -0600

    Update support test

--- generator/src/template.rs
+++ generator/src/template.rs
@@ -294,7 +294,7 @@ mod test {
         let out = t.compile(Default::default());
         let want = "let url =
 format!(\"/measure/{}\",
-crate::progenitor_support::encode_path(&number.to_string()),);\n";
+crate::progenitor_support::encode_path(number),);\n";
         assert_eq!(want, &out);
         Ok(())
     }
