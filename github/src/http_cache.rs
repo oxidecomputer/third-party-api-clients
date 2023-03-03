c748ae1dd5196ca085fddbd35c8af844d73b5f29
Emil Eriksson <8700261+ginger51011@users.noreply.github.com>
2023-03-03 16:47:01 +0100

    [github] Add `HttpCache::in_dir` constructor to allow for custom cache placement (#59)
    * Add HttpCache::in_dir constructor to allow for custom cache placement
    
    Before only the home dir could be used, this makes it a bit more ergonomic
    if the user has other desires
    
    * Use HttpCache::in_dir for HttpCache::in_home_dir

--- github/src/http_cache.rs
+++ github/src/http_cache.rs
@@ -35,9 +35,17 @@ impl dyn HttpCache {
     }
 
     pub fn in_home_dir() -> BoxedHttpCache {
-        let mut dir = dirs::home_dir().expect("Expected a home dir");
-        dir.push(".github/cache");
-        Box::new(FileBasedCache::new(dir))
+        Self::in_dir(&dirs::home_dir().expect("Expected a home dir"))
+    }
+
+    pub fn in_dir(path: &Path) -> BoxedHttpCache {
+        if path.is_dir() {
+            let mut path = path.to_owned();
+            path.push(".github/cache");
+            Box::new(FileBasedCache::new(path))
+        } else {
+            panic!("Expected a dir");
+        }
     }
 }
 
